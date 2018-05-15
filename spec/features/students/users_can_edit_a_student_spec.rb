e66bc99c24abb39931c0b7bb13e1182f49b815e6
Eliot Swank <eliotswank@gmail.com>
2018-05-15 14:29:45 -0600

    adds student name edit functionality

--- spec/features/students/users_can_edit_a_student_spec.rb
+++ spec/features/students/users_can_edit_a_student_spec.rb
@@ -5,12 +5,12 @@ describe "a user visits the edit page to edit a student," do
     it "redirects me to the student show page and shows the student's new name" do
       student = Student.create(id: 1, name: "Foobar Baz")
 
-      visit edit_student_path
+      visit edit_student_path(student)
 
       fill_in "student[name]",	with: "Zab Raboof"
       click_on "Update"
 
-      expect(current_path).to eq("/students/{Student.last.id}")
+      expect(current_path).to eq("/students/#{Student.last.id}")
       expect(page).to have_content("Zab Raboof")
       expect(page).to_not have_content("Foobar Baz")
     end
