6fe80adbde589d8c9a0ba7f19d1bdb4a80373911
Eliot Swank <eliotswank@gmail.com>
2018-05-15 14:27:34 -0600

    adds test to edit a student

---
+++ spec/features/students/users_can_edit_a_student_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "a user visits the edit page to edit a student," do
+  describe "fills out the form and clicks submit" do
+    it "redirects me to the student show page and shows the student's new name" do
+      student = Student.create(id: 1, name: "Foobar Baz")
+
+      visit edit_student_path
+
+      fill_in "student[name]",	with: "Zab Raboof"
+      click_on "Update"
+
+      expect(current_path).to eq("/students/{Student.last.id}")
+      expect(page).to have_content("Zab Raboof")
+      expect(page).to_not have_content("Foobar Baz")
+    end
+    
+  end
+  
+end
