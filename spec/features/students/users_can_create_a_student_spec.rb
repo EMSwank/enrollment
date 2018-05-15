79b37455a0195c66abfc464f8b0e7b4a9665ad39
Eliot Swank <eliotswank@gmail.com>
2018-05-15 14:19:22 -0600

    adds student creation feature with test--GREEN

---
+++ spec/features/students/users_can_create_a_student_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "A user visits /students/new" do
+  it "and creates a student by filling out a form and clicking submit" do
+    visit new_student_path
+
+    fill_in "student[name]",	with: "Foobar Baz"
+    click_on "Create"
+
+    expect(current_path).to eq("/students/#{Student.last.id}")
+    expect(page).to have_content('Foobar Baz')
+    expect(Student.count).to eq(1)
+  end
+  
+end
